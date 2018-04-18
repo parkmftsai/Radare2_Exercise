f91b91f970d3e12f114ceb9d8a426a494b9bf474
parkmftsai <parktasi@gmail.com>
2018-04-18 15:14:23 +0800

    source c code

---
+++ testc.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int i;
+    printf("please input number:");
+    scanf("%d",&i);
+    if(i%2==0)
+      puts("even");
+    else
+      puts("odd");
+
+
+return 0;
+}
